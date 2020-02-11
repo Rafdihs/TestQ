a05d163b87404c432b3070ec5211eb4d687ba8b3
Rafdihs <60420056+Rafdihs@users.noreply.github.com>
2020-02-11 12:51:11 +0700

    Add files via upload

---
+++ Company.php
@@ -0,0 +1,26 @@
+<?php
+class company {
+}
+ $namacom="DOT";
+
+ class department {
+}
+ $namadep="IT";
+
+
+ class employee {
+}
+ $nama="Rafdi";
+ $tittle="Quality Assurance";
+ $salary=40000;
+
+
+
+echo "Nama Perusahaan ". $namacom;
+echo "<p>Divisi ". $namadep;
+echo "<p>Nama ". $nama;
+echo "<p>Berposisi ". $tittle;
+echo "<p>Salary ". $salary;
+
+
+?>
